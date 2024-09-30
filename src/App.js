6d0bde75ad4d4cce9df6c1c4a62b824d19733543
Nargot <106655199+Nargot1@users.noreply.github.com>
2024-09-30 09:23:57 +0200

    logo spin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Witaj from './Witaj';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Witaj/>
       </header>
     </div>
   );
---
+++ src/Witaj.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Powitanie = () => {
+    return (
+        <h1>Witaj</h1>
+    )
+};
+
+export default Powitanie;
